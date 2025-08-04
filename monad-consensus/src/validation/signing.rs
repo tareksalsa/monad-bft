2e681b4e8fda600096603f88395b82fb83ce1009
Aashish Welling <aashishwelling@gmail.com>
2025-08-04 10:14:37 -0400

    Fast-fail if NE obviously fails signature checks

--- monad-consensus/src/validation/signing.rs
+++ monad-consensus/src/validation/signing.rs
@@ -706,6 +706,10 @@ where
 {
     /// A valid timeout message is well-formed, and carries valid QC/TC
     pub fn validate(&self, epoch_manager: &EpochManager) -> Result<(), Error> {
+        if self.signature.validate().is_err() {
+            return Err(Error::InvalidSignature);
+        }
+
         self.well_formed_no_endorsement()?;
         self.verify_epoch(epoch_manager)?;
 
@@ -1172,6 +1176,7 @@ mod test {
             ConsensusBlockHeader, MockExecutionBody, MockExecutionProposedHeader,
             MockExecutionProtocol,
         },
+        no_endorsement::NoEndorsement,
         payload::{ConsensusBlockBody, ConsensusBlockBodyInner, RoundSignature},
         quorum_certificate::QuorumCertificate,
         timeout::{HighExtend, HighTipRoundSigColTuple, TimeoutCertificate, TimeoutInfo},
@@ -1208,7 +1213,7 @@ mod test {
 
     use super::{verify_qc, verify_tc, Verified};
     use crate::{
-        messages::message::{ProposalMessage, TimeoutMessage},
+        messages::message::{NoEndorsementMessage, ProposalMessage, TimeoutMessage},
         validation::{
             certificate_cache::CertificateCache,
             signing::{epoch_to_validators, VoteMessage},
@@ -2128,5 +2133,35 @@ mod test {
             &tc,
         );
         assert_eq!(tc_verify_result, Err(Error::InvalidEpoch));
+    }
+
+    #[test]
+    fn ne_invalid_signature() {
+        type SignatureCollectionType =
+            BlsSignatureCollection<CertificateSignaturePubKey<SignatureType>>;
+
+        let not_in_subgroup_bytes: [u8; 96] = [
+            0xac, 0xb0, 0x12, 0x4c, 0x75, 0x74, 0xf2, 0x81, 0xa2, 0x93, 0xf4, 0x18, 0x5c, 0xad,
+            0x3c, 0xb2, 0x26, 0x81, 0xd5, 0x20, 0x91, 0x7c, 0xe4, 0x66, 0x65, 0x24, 0x3e, 0xac,
+            0xb0, 0x51, 0x00, 0x0d, 0x8b, 0xac, 0xf7, 0x5e, 0x14, 0x51, 0x87, 0x0c, 0xa6, 0xb3,
+            0xb9, 0xe6, 0xc9, 0xd4, 0x1a, 0x7b, 0x02, 0xea, 0xd2, 0x68, 0x5a, 0x84, 0x18, 0x8a,
+            0x4f, 0xaf, 0xd3, 0x82, 0x5d, 0xaf, 0x6a, 0x98, 0x96, 0x25, 0xd7, 0x19, 0xcc, 0xd2,
+            0xd8, 0x3a, 0x40, 0x10, 0x1f, 0x4a, 0x45, 0x3f, 0xca, 0x62, 0x87, 0x8c, 0x89, 0x0e,
+            0xca, 0x62, 0x23, 0x63, 0xf9, 0xdd, 0xb8, 0xf3, 0x67, 0xa9, 0x1e, 0x84,
+        ];
+
+        let epoch_manager = EpochManager::new(SeqNum(2000), Round(50), &[(Epoch(1), Round(0))]);
+
+        let ne_msg: NoEndorsementMessage<SignatureCollectionType> = NoEndorsementMessage {
+            msg: NoEndorsement {
+                epoch: Epoch(1),
+                round: Round(10),
+                tip_qc_round: Round(9),
+            },
+            signature: BlsSignature::uncompress(&not_in_subgroup_bytes).unwrap(),
+        };
+
+        let maybe_validated = ne_msg.validate(&epoch_manager);
+        assert_eq!(maybe_validated, Err(Error::InvalidSignature));
     }
 }
